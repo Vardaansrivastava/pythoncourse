6521cad6c843e81f1b93a95d56be4e4ae9ee5ab0
Vardaansrivastava <jyotisengarsrivastava@gmail.com>
2026-06-11 12:09:54 +0530

    lesson4

---
+++ python_activities/lesson 4/main.py
@@ -0,0 +1,46 @@
+# Operators: They are symbols that perform mathematical, logical or assignment operations on values called operands.
+# Operands: They are values or variables on which operators act.
+
+#  Types of operators:
+# 1. Arithemetic operators: They perform mathematical calculations such as addition, substraction, multiplication, division and more. +: addition, -: substraction, *: multiplication, / : division, % : modulus, **: exponentiation, //: floor division. 
+# Example:
+
+# print(3+5)
+# print(5-3)
+# print(3*5)
+# print(6/2) # gives result in float value.
+# print(3**2)
+# print(6//4) # gives the quotient.
+# print(15%5)
+
+# 2. Comparison operators: They are the operators used to compare two operands and return either True or False. ==: Equal to , !=: not equal to, >: greater than, <: less than, >= : greater than or equal to, <= less than or equal to. They are used for making decisions.
+# Example:
+
+# print(3==3)
+# print(4!=3)
+# print(4>3)
+# print(3<4)
+# print(3>=3)
+# print(3<=3)
+
+
+# 3. Assignment operators: They are used to assign values to variables or update the current values of variables with new values, often combining arithmetic or bitwise operations. =: simple assignment, +=: add and assign, -=: subtract and assign, *=: multiple and assign, /=: divide and assign, %= : modulus and assign, //=: floor division and assign, **= : exponentiate and assign.
+# Example:
+a=5
+print(a)
+a+=3 #5
+print(a)
+a-=3 #2
+print(a)
+a*=3
+print(a)
+a/=3
+print(a)
+a%=3 #2
+print(a)
+a**=3
+print(a)
+a//=3
+print(a)
+
+# 4. Logical operators: They are used for logical operations such as and, or and not.
