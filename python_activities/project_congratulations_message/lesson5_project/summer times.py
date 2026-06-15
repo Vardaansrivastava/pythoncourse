463518e312583b2f08177e46da1311318882c00a
Vardaansrivastava <jyotisengarsrivastava@gmail.com>
2026-06-15 21:56:55 +0530

    checking temps

---
+++ python_activities/project_congratulations_message/lesson5_project/summer times.py
@@ -0,0 +1,6 @@
+# WAP to check if the temperature is suitable for wearing light clothes or not.
+user_input = int(input("Enter the temperature(in celsius) :"))
+if user_input > 25:
+    print("The temperature is suitable for wearing light clothes.")
+else:
+    print("The temperature is not suitable for wearing light clothes.")
