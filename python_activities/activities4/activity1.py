97e2682e4d90c548209f9ca8064cbacdbafac708
Vardaansrivastava <jyotisengarsrivastava@gmail.com>
2026-06-15 15:24:22 +0530

    stored birthdays

---
+++ python_activities/activities4/activity1.py
@@ -0,0 +1,14 @@
+# Storing Values
+tree1 = 35
+tree2 = 73
+tree3 = 12
+tree4 = 84
+tree5 = 96
+
+# total of all the treees
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all the 5 trees is: ", sum)
+
+# average of trees
+average = sum/5
+print("the average of all the tree is :", average)
---
+++ python_activities/lesson5/main.py
@@ -0,0 +1,52 @@
+# Indentation: It hightlights a block of code, it also tell the python interpreter that this group of statements belong to a particular block of code.
+
+# Conditional statements are decision making statements, in them if a condition is tested and it remains true or false, then the other block of code is executed.
+
+
+
+
+
+# proving if statement
+# a = 3
+# if a<=5:
+#     print("a is less than or equal to 5")
+    
+#     # WAP to check if a number is positive.
+#     a=12
+#     if a>0:
+#         print("a is a positive number")
+
+
+    # WAP to check if a person is buying oranges at 100 ruppes and later selling it at 120 ruppes, find if he has a profit or loss.
+    # cp = 150
+    # sp = 120
+    # if sp > cp:
+    #     print("The person has a profit.")
+    # else:
+    #     print("the person has a loss.:")
+
+# WAP to check if a number is greater or smaller.
+# user_input = int(input("enter 1st number :"))
+# user_input2 = int(input("enter 2nd number :"))
+# if user_input > user_input2:
+#     print("1st number is greater than the 2nd number.")
+# else:
+#     print("2nd number is greater than the 1st number")
+
+
+# WAP to check if a number is odd or even.
+# user_input1 = int(input("enter a number :"))
+# if user_input1 % 2==0:
+#     print("the number given by the user is even")
+# else:
+#     print("the number given by the user is odd")
+
+
+a=int(input("Enter a number between 5 to 50: ")) #25    
+b=int(input("Enter a number between 51 to 95:  ")) #15
+res=a/b
+if(res>0):
+    print(True,res)
+else:
+    print(False,res)
+
