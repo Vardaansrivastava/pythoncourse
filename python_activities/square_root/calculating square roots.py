b6432756075d38546ff1c136a9ade1e8f0123984
Vardaansrivastava <jyotisengarsrivastava@gmail.com>
2026-06-15 22:06:04 +0530

    square root finder

---
+++ python_activities/square_root/calculating square roots.py
@@ -0,0 +1,4 @@
+# Calculating square roots.
+user_input = int(input("Enter a number to find its its square root :"))
+user_input_sqrt = user_input ** 0.5
+print("The square root of ", user_input, " is ", user_input_sqrt)
