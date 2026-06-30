a3fdca9837d8fac283cabb16590ecc28ad34b20e
Vardaansrivastava <jyotisengarsrivastava@gmail.com>
2026-06-30 10:04:37 +0530

    pr11

---
+++ python_activities/project/project_11.py
@@ -0,0 +1,10 @@
+# WAP to make a mirrored right triangle. I experimented a bit.
+n = int(input("Enter the number of rows: "))
+
+for i in range(1, n + 1):
+    print(" " * (n - i) + "*" * i)
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i) + "*" * i)
+
+
+
